9e09deb182a82c130fdeb27edc4a79b0b35feb3d
Russell Gk <ruslgee@gmail.com>
2015-12-03 02:52:01 +0300

    Add the onCreateView() logging

--- app/src/main/java/com/hardteam/moneytracker/MainActivity.java
+++ app/src/main/java/com/hardteam/moneytracker/MainActivity.java
@@ -1,10 +1,12 @@
 package com.hardteam.moneytracker;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.Menu;
@@ -30,6 +32,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         Log.d(LOG_VIEW, "onCreate()");
+    }
+
+    @Override
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        Log.d(LOG_VIEW,"onCreateView()");
+        return super.onCreateView(name, context, attrs);
     }
 
     @Override
