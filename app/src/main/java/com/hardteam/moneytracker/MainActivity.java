2e33833b3058b9ac72b497bbdfc1077b296adeb3
Russell Gk <ruslgee@gmail.com>
2015-12-05 06:32:07 +0300

    Removed Toolbar to separated file, cleaned some code in MainActivity.java

--- app/src/main/java/com/hardteam/moneytracker/MainActivity.java
+++ app/src/main/java/com/hardteam/moneytracker/MainActivity.java
@@ -19,68 +19,20 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_VIEW = MainActivity.class.getSimpleName();
-    private Toolbar toolbar;
     private DrawerLayout drawerLayout;
-    private CoordinatorLayout coordinatorContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        coordinatorContainer = (CoordinatorLayout) findViewById(R.id.coordinator_container);
         setupToolbar();
         setupDrawer();
         Log.d(LOG_VIEW, "onCreate()");
     }
 
-    @Override
-    public View onCreateView(String name, Context context, AttributeSet attrs) {
-        Log.d(LOG_VIEW,"onCreateView()");
-        return super.onCreateView(name, context, attrs);
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(LOG_VIEW, "onRestart()");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(LOG_VIEW, "onStart()");
-    }
-
-    @Override
-    protected void onResume()
-    {
-        super.onResume();
-        Log.d(LOG_VIEW, "onResume()");
-    }
-
-    //onFreeze()
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(LOG_VIEW, "onPause()");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(LOG_VIEW, "onStop()");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(LOG_VIEW, "onDestroy()");
-    }
-
     private void setupToolbar()
     {
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();//check the import android.support.v7.app.ActionBar
         if(actionBar != null)
@@ -99,33 +51,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(MenuItem item)
             {
-                Snackbar.make(coordinatorContainer, item.getTitle(), Snackbar.LENGTH_SHORT).show();
                 item.setChecked(true);
                 drawerLayout.closeDrawers();
                 return false;
             }
         });
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        return true;
-//    }
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        // Handle action bar item clicks here. The action bar will
-//        // automatically handle clicks on the Home/Up button, so long
-//        // as you specify a parent activity in AndroidManifest.xml.
-//        int id = item.getItemId();
-//
-//        //noinspection SimplifiableIfStatement
-//        if (id == R.id.action_settings) {
-//            return true;
-//        }
-//
-//        return super.onOptionsItemSelected(item);
-//    }
 }
