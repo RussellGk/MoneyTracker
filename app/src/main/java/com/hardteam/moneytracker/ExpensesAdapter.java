8a9b2fab977b6deeac10aae317a2f9acb55e5e9f
Russell Gk <ruslgee@gmail.com>
2015-12-07 15:09:12 +0300

    Add the color for ExpensesAdapter

--- app/src/main/java/com/hardteam/moneytracker/ExpensesAdapter.java
+++ app/src/main/java/com/hardteam/moneytracker/ExpensesAdapter.java
@@ -5,9 +5,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import java.util.List;
+
+import static android.graphics.Color.rgb;
 
 /**
  * Created by RG on 06.12.2015.
@@ -15,6 +18,7 @@ import java.util.List;
 public class ExpensesAdapter extends ArrayAdapter<Expense> {
 
     List<Expense> expenses;
+    static int foneColor;
 
     public ExpensesAdapter(Context context, List<Expense> expenses) {
         super(context,0,expenses);
@@ -25,11 +29,16 @@ public class ExpensesAdapter extends ArrayAdapter<Expense> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent)
     {
+
         Expense expense = getItem(position);
         if(convertView == null)
         {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item, parent,false);
         }
+
+        RelativeLayout fone = (RelativeLayout) convertView.findViewById(R.id.relative_item);
+        foneColor = rgb(getIntColor(),getIntColor(),getIntColor());
+        fone.setBackgroundColor(foneColor);
         TextView name =(TextView) convertView.findViewById(R.id.name_text);
         TextView data =(TextView) convertView.findViewById(R.id.date_text);
         TextView sum = (TextView) convertView.findViewById(R.id.sum_text);
@@ -40,5 +49,12 @@ public class ExpensesAdapter extends ArrayAdapter<Expense> {
         data.setText(expense.getHumanDate(expense.getDate()));
         sum.setText(expense.getMoney(expense.getSum()));
         return convertView;
+    }
+
+    private static int getIntColor()
+    {
+       int rnd = 0 + (int)(Math.random() * ((255 - 0) + 1));
+
+        return rnd;
     }
 }
